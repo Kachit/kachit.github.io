880da9fe966a61066ba3c10ca85b885d182f130e
Kachit <antoxachaos@gmail.com>
2015-01-16 15:15:29 +0300

    123

--- js/app.js
+++ js/app.js
@@ -1,5 +1,67 @@
+'use strict';
+
 var app = angular.module('app', []);
 
-app.controller('MainCtrl', ['$scope', function ($scope){
+app.controller('newsCtrl', ['$scope', function($scope) {
 
+    $scope.list = false;
+    $scope.message = 'Hover Me!!!';
+    $scope.posts = [
+        {
+            id: 1,
+            title: 'name1',
+            active: false,
+            text: 'text1'
+        },
+        {
+            id: 2,
+            title: 'name2',
+            active: false,
+            text: 'text3'
+        },
+        {
+            id: 3,
+            title: 'name3',
+            active: true,
+            text: 'text3'
+        },
+        {
+            id: 5,
+            title: 'name5',
+            active: false,
+            text: 'text5'
+        },
+        {
+            id: 6,
+            title: 'name6',
+            active: true,
+            text: 'text6'
+        },
+        {
+            id: 4,
+            title: 'name4',
+            active: true,
+            text: 'text4'
+        }
+    ];
+
+    $scope.getFileName = function() {
+        return ($scope.list) ? '/html/list.html' : '/html/table.html';
+    };
+
+    $scope.addNewUser = function() {
+        alert(123);
+    };
+
+    $scope.newUser = {};
+
+    $scope.message = "Ready";
 }]);
+
+app.directive('tap', function(){
+    return function (scope, elem, attrs){
+        elem.on('mouseenter', function() {
+            scope.$apply(attrs['tap']);
+        });
+    }
+});
